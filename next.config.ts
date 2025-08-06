fab0d02ff9e6e804922c58f27622ca515c708205
Diaa <devmodsup@gmail.com>
2025-08-06 18:05:13 +0000

    Error: Invalid src prop (https://live.staticflickr.com/8446/8008884577_8

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'live.staticflickr.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
