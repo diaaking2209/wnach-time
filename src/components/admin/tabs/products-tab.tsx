ada3f586bc7976dc78dac25d4ba3da65235fb3c3
Diaa <devmodsup@gmail.com>
2025-08-07 23:55:23 +0000

    ReferenceError: AlertDialogTrigger is not defined     at https://6000-fi

--- src/components/admin/tabs/products-tab.tsx
+++ src/components/admin/tabs/products-tab.tsx
@@ -38,6 +38,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useToast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
