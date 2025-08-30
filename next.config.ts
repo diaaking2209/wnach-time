29fc8348031ba268097e5d195201719064eb076f
Diaa <devmodsup@gmail.com>
2025-08-30 21:23:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.discordapp.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'l.top4top.io',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
