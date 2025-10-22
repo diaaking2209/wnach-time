804e06e0410791fd3d490ab902221a156609606d
Diaa <devmodsup@gmail.com>
2025-10-22 10:33:56 +0000

    11:32:46 AM: Netlify Build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,22 +1,11 @@
 
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-let supabaseInstance: SupabaseClient | null = null;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const getSupabase = () => {
-    if (supabaseInstance) {
-        return supabaseInstance;
-    }
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL and Anon Key must be provided in your environment variables.");
+}
 
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-    
-    if (!supabaseUrl || !supabaseAnonKey) {
-        throw new Error("Supabase URL and Anon Key must be provided in your environment variables.");
-    }
-    
-    supabaseInstance = createClient(supabaseUrl, supabaseAnonKey);
-    return supabaseInstance;
-};
-
-export const supabase = getSupabase();
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
